4ae8977c8cf823382e2290bcc589cc761522c9d2
marciafof <mfotaloraf@gmail.com>
2023-02-23 14:45:04 +0100

    Dropped the duplicated rows based on track_id

--- concatenating_database.py
+++ concatenating_database.py
@@ -8,7 +8,7 @@ data_path = "data"
 files_path = f"{data_path}/by_genre"
 
 #%%Searching for files recursively (because of the genre folders)
-concat_by_genre = False
+concat_by_genre = True
 if concat_by_genre:
     print(f"Concatenating files inside by_genre directory and subfolders")
     list_of_dfs = []
@@ -16,10 +16,13 @@ if concat_by_genre:
         print(file_)
         list_of_dfs.append(pd.read_csv(file_))
     df_by_genre = pd.concat(list_of_dfs,axis=0)
+    nduplicates = df_by_genre[df_by_genre.duplicated(subset="id")].shape[0]
+    print(f"Number of duplicated track id's is {nduplicates}")
+    df_by_genre.drop_duplicates(subset="id",inplace=True)
     df_by_genre.to_csv(data_path + "/songs_by_genre.csv",index=False)
 
 #%% Searching for all csv files for playlists
-concat_by_playlists = False
+concat_by_playlists = True
 if concat_by_playlists:
     print(f"Concatenating files inside by_playlist directory")
     files_path = f"{data_path}/by_playlist"
@@ -33,6 +36,9 @@ if concat_by_playlists:
             continue
 
     all_playlists_df = pd.concat(list_of_dfs)
+    nduplicates = all_playlists_df[all_playlists_df.duplicated(subset="id")].shape[0]
+    print(f"Number of duplicated track id's is {nduplicates}")
+    all_playlists_df.drop_duplicates(subset="id",inplace=True)
     all_playlists_df.to_csv(data_path + "/songs_by_our_playlists.csv",index=False)
 # %%ADD songs dowloaded from other sources and combine by genre
 fname_1 = data_path +"/songs_by_genre.csv"
@@ -44,4 +50,8 @@ for fname_ in lists_of_fnames:
     print(f"Opening file {fname_}")
     list_of_dfs.append(pd.read_csv(fname_))
 final_df = pd.concat(list_of_dfs)
+nduplicates = final_df[final_df.duplicated(subset="id")].shape[0]
+print(f"Number of duplicated track id's is {nduplicates}")
+final_df.drop_duplicates(subset="id",inplace=True)
+print(f"Final number of rows in dataframe is {final_df.shape[0]}")
 final_df.to_csv(data_path + "/all_songs_attributes.csv",index=False)
